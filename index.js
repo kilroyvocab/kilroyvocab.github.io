759609048e863005ba5be45edaae9602da6e04af
corey2915 <32797309+corey2915@users.noreply.github.com>
2018-01-29 14:12:42 -0500

    clean up

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function setup(){
-  createCanvas(1400,750);
-  console.log(mouseX, mouseY);
-}
 
-function draw(){
-  drawIntor()
-}
-
-function drawIntor(){
-  // draw the sentence welcome to...
-  var w = 'Welcome To Ms.Kilroy Global History Vocabulary Website';
-  fill(0);
-  textSize(40);
-  text(w,50,300);
-
-}
